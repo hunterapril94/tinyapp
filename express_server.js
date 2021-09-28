fa1e2a3a0c82f1647d91c79e285c27754f3e590d
April Larsen <hunterapril94@gmail.com>
2021-09-28 00:58:44 +0000

    created a new urls page in which someone submits long url for shortening

--- express_server.js
+++ express_server.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const getRandomString = function () {
+  let random = ""
+  const options = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  for(let i = 0; i < 5; i++) {
+    num = Math.floor(Math.random() * 62)
+    random += options(num)
+  }
+  return random;
+}
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
